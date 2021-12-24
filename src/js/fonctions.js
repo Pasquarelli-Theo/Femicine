985e83c709686007a2a178b2f5d59eb2a731581a
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2021-12-24 12:26:10 +0100

    commit carrousel qui marche pas

--- src/js/fonctions.js
+++ src/js/fonctions.js
@@ -10,13 +10,13 @@ function carrousel() {
 
     let animfilms = anime.timeline({
         loop: true,
-        delay: 0
+        delay: 2
     });
 
     animfilms.add({
         targets: imagesfilms,
-        translateX: [{value: '500', duration: d},
-                     {value: '-500', duration: 0, delay: function(img,ind) {
+        translateX: [{value: '242', duration: d},
+                     {value: '-242', duration: 0, delay: function(img,ind) {
                          if (ind == 0) return (5*delta)+(4*d); 
                          if (ind == 1) return (4*delta)+(3*d);
                          if (ind == 2) return (3*delta)+(2*d);
@@ -35,8 +35,9 @@ function carrousel() {
             if (ind == 2) return (3*delta)+(2*d);
             if (ind == 3) return (4*delta)+(3*d);
             if (ind == 4) return (5*delta)+(4*d);
-            if (ind == 4) return (6*delta)+(5*d);
-            return (7*delta)+(6*d);
+            if (ind == 5) return (6*delta)+(5*d);
+            if (ind == 6) return (7*delta)+(6*d);
+            return (8*delta)+(7*d);
         }
     });	
 }
